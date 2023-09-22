f2dadec6edcfd9c8f83c047cfefdcf5a3dab29dd
Swann_Jptz <63155968+RazAallgull60@users.noreply.github.com>
2023-09-22 04:01:40 +0200

    add fix

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,24 +1,22 @@
-'use client';
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 interface AvatarProps {
   src: string | null | undefined;
-};
-
-const Avatar: React.FC<AvatarProps> = ({
-  src
-}) => {
-  return (
-    <Image 
-        className="rounded-full"
-        height="30"
-        width="30"
-        alt="Avatar"
-        src={src || "/images/placeholder.jpg"}
-    />
-  )
 }
 
-export default Avatar
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return (
+    <Image
+      className="rounded-full"
+      height="30"
+      width="30"
+      alt="Avatar"
+      src={src || "/images/placeholder.jpg"}
+    />
+  );
+};
+
+export default Avatar;
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,96 +1,80 @@
-'use client'
+"use client";
 
-import React, { useCallback, useState } from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
-import Avatar from '../Avatar'
-import MenuItem from './MenuItem'
-import useRegisterModal from '@/app/hooks/useRegisterModal';
-import useLoginModal from '@/app/hooks/useLoginModal'
+import React, { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
-import { signOut } from 'next-auth/react'
-import { SafeUser } from '@/app/types'
+import { signOut } from "next-auth/react";
+import { SafeUser } from "@/app/types";
 
 interface UserMenuProps {
-    currentUser?: SafeUser | null
+  currentUser?: SafeUser | null;
 }
 
-const UserMenu: React.FC<UserMenuProps> = ({
-    currentUser
-}) => {
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
-    const [isOpen, setIsOpen] = useState(false);
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleOpen = useCallback(() => {
-        setIsOpen((value) => !value);
-    }, []);
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
 
   return (
     <div className="relative">
-        <div className="flex flex-row items-center gap-3">
-            <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full 
-            hover:bg-neutral-100 transition cursor-pointer">
-                Airbnb votre chez vous
-            </div>
-            <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px]
-            border-neutral-200 flex flex-row items-center gap-3 rounded-full
-            cursor-pointer hover:shadow-md transition">
-                <AiOutlineMenu />
-                <div className="hidden md:block">
-                    <Avatar src={currentUser?.image}/>
-                </div>
-            </div>           
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full 
+            hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Airbnb votre chez vous
         </div>
-        {isOpen && (
-            <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white
-            overflow-hidden right-0 top-12 text-sm">
-                <div className="flex flex-col cursor-pointer">
-                    {currentUser ? (
-                    <>
-                        <MenuItem 
-                            onClick={() => {}}
-                            label="Mes voyages" 
-                        />
-                        <MenuItem 
-                            onClick={() => {}}
-                            label="Mes favoris"
-                        />
-                        <MenuItem 
-                            onClick={() => {}}
-                            label="Mes réservations"
-                        />
-                        <MenuItem 
-                            onClick={() => {}}
-                            label="Mes propriétés"
-                        />
-                        <MenuItem 
-                            onClick={() => {}}
-                            label="Airbnb mon chez moi"
-                        />
-                        <hr />
-                        <MenuItem 
-                            onClick={() => signOut()}
-                            label="Déconnexion"
-                        />
-                    </>
-
-                    ) : (
-                        <>
-                            <MenuItem 
-                                onClick={loginModal.onOpen}
-                                label="Se connecter" 
-                            />
-                            <MenuItem 
-                                onClick={registerModal.onOpen}
-                                label="Créer un compte"
-                            />
-                        </>
-                    )}                    
-                </div>
-            </div>
-        )}
+        <div
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px]
+            border-neutral-200 flex flex-row items-center gap-3 rounded-full
+            cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div
+          className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white
+            overflow-hidden right-0 top-12 text-sm"
+        >
+          <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+                <MenuItem onClick={() => {}} label="Mes voyages" />
+                <MenuItem onClick={() => {}} label="Mes favoris" />
+                <MenuItem onClick={() => {}} label="Mes réservations" />
+                <MenuItem onClick={() => {}} label="Mes propriétés" />
+                <MenuItem onClick={() => {}} label="Airbnb mon chez moi" />
+                <hr />
+                <MenuItem onClick={() => signOut()} label="Déconnexion" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={loginModal.onOpen} label="Se connecter" />
+                <MenuItem
+                  onClick={registerModal.onOpen}
+                  label="Créer un compte"
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UserMenu
+export default UserMenu;
